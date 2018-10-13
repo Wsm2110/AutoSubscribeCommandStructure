aadd0702349247c6d16666411561715d2618782d
Dev01\Wiljan <wruizendaal@gmail.com>
2018-10-13 22:45:37 +0200

    ....
    CHG: remove project references, load all modules dynamically @ runtime - integrated with autofac... etc

---
+++ Entities/Extentions/ContainerBuilderExtentions.cs
@@ -0,0 +1,92 @@
+﻿using Autofac;
+using Autofac.Builder;
+using Entities.Arguments;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Extentions
+{
+    public static class ContainerBuilderExtentions
+    {
+
+        /// <summary>
+        /// Registers the name of the instance by type.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException">
+        /// type
+        /// or
+        /// instance
+        /// </exception>
+        public static IRegistrationBuilder<dynamic, SimpleActivatorData, SingleRegistrationStyle> RegisterInstanceByTypeName(this ContainerBuilder builder, string name, object[] args = null)
+        {
+            var type = GetModules().SelectMany(item => item.DefinedTypes).FirstOrDefault(t => t.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+            if (type == null)
+            {
+                throw new NullReferenceException(nameof(type));
+            }
+
+            var instance = Activator.CreateInstance(type, args);
+
+            if (instance == null)
+            {
+                throw new NullReferenceException(nameof(instance));
+            }
+
+            return builder.RegisterInstance(instance);
+        }
+
+
+
+        /// <summary>
+        /// Traversals the specified dir.
+        /// </summary>
+        /// <param name="dir">The dir.</param>
+        /// <param name="count">The count.</param>
+        /// <returns></returns>
+        /// <exception cref="System.IO.DirectoryNotFoundException">Unable to found module directory</exception>
+        public static DirectoryInfo Traversal(DirectoryInfo dir, int count)
+        {
+            if (count == 5)
+            {
+                throw new DirectoryNotFoundException("Unable to find [module] directory");
+            }
+
+            var module = dir.Parent.GetDirectories().FirstOrDefault(d => d.Name.Equals("Modules", StringComparison.OrdinalIgnoreCase));
+            if (module != null)
+            {
+                return module;
+            }
+
+            count++;
+
+            return Traversal(dir.Parent, count);
+        }
+
+        /// <summary>
+        /// Gets the modules.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Assembly> GetModules()
+        {
+            var dir = new DirectoryInfo(Environment.CurrentDirectory);
+            var moduleDir = Traversal(dir.Parent, 0);
+
+            var result = new List<Assembly>();
+
+            moduleDir.GetFiles("*.dll").Apply(item => result.Add(Assembly.LoadFrom(item.FullName)));
+
+            return result;
+        }
+
+    }
+}
--- TestRunnerArchitecture/Program.cs
+++ TestRunnerArchitecture/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using Entities.Arguments;
 using Entities.Contracts;
+using Entities.Extentions;
 using Entities.Models;
 using System;
 using System.Collections.Generic;
@@ -21,7 +22,7 @@ namespace TestRunnerArchitecture
         {
             subCommand = new List<SubCommand>();
 
-            IoCInitializer();     
+            IoCInitializer();
 
             Console.ReadKey(true);
         }
@@ -39,71 +40,33 @@ namespace TestRunnerArchitecture
                    .AsImplementedInterfaces();
 
             // Scan modules folder
-            GetModules().Apply(item => builder.RegisterAssemblyTypes(item)
+            ContainerBuilderExtentions.GetModules().Apply(item => builder.RegisterAssemblyTypes(item)
                                               .AsImplementedInterfaces());
 
+            // create instance of eventaggregator
             var eventAggregator = new EventAggregator();
 
+            //register eventaggregator
             builder.RegisterInstance(eventAggregator)
                    .As<IEventAggregator>();
-       
+
             //Inorder for the eventaggregator to work we have to subscribe before commands are activated
             eventAggregator.Subscribe(new Program());
 
-            builder.RegisterType<VMwareCommand.VmwareCommand>()                             
+            //resolve modules dynamically without using project references
+            builder.RegisterInstanceByTypeName("VmwareCommand", new object[] { eventAggregator })
                    .SingleInstance()
                    .Named("c1", typeof(ICommandBuilder))
-                   .OnActivated(args => args.Instance.Initialize()) 
+                   .OnActivated(args => (args.Instance as ICommandBuilder).Initialize())
                    .AutoActivate();
-            
-            builder.RegisterType<InfraModule.InfraCommand>()                            
+
+            builder.RegisterInstanceByTypeName("InfraCommand", new object[] { eventAggregator })
                    .SingleInstance()
                    .Named("c2", typeof(ICommandBuilder))
-                   .OnActivated(args => args.Instance.Initialize())
+                   .OnActivated(args => (args.Instance as ICommandBuilder).Initialize())
                    .AutoActivate();
 
             _container = builder.Build();
-        }
-
-        /// <summary>
-        /// Gets the modules.
-        /// </summary>
-        /// <returns></returns>
-        private static IEnumerable<Assembly> GetModules()
-        {
-            var dir = new DirectoryInfo(Environment.CurrentDirectory);
-            var moduleDir = Traversal(dir.Parent, 0);
-
-            var result = new List<Assembly>();
-
-            moduleDir.GetFiles("*.dll").Apply(item => result.Add(Assembly.LoadFrom(item.FullName)));
-
-            return result;
-        }
-
-        /// <summary>
-        /// Traversals the specified dir.
-        /// </summary>
-        /// <param name="dir">The dir.</param>
-        /// <param name="count">The count.</param>
-        /// <returns></returns>
-        /// <exception cref="System.IO.DirectoryNotFoundException">Unable to found module directory</exception>
-        private static DirectoryInfo Traversal(DirectoryInfo dir, int count)
-        {
-            if (count == 5)
-            {
-                throw new DirectoryNotFoundException("Unable to find [module] directory");
-            }
-
-            var module = dir.Parent.GetDirectories().FirstOrDefault(d => d.Name.Equals("Modules", StringComparison.OrdinalIgnoreCase));
-            if (module != null)
-            {
-                return module;
-            }
-
-            count++;
-
-            return Traversal(dir.Parent, count);
         }
 
         /// <summary>
